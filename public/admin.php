e8e1fd957a485b684823bd39fba427e662baee82
luogreen <luogreen@foxmail.com>
2017-07-07 15:14:06 +0800

    修改Admin为小写

--- public/admin.php
+++ public/admin.php
@@ -1,5 +1,5 @@
 <?php
 header('Pragma: no-cache', false);
 require dirname(__DIR__) . '/conf/Init.php';
-$app  = new \Yaf\Application(APP_CONF. '/Admin.ini');
+$app  = new \Yaf\Application(APP_CONF. '/admin.ini');
 $app->bootstrap()->run();
